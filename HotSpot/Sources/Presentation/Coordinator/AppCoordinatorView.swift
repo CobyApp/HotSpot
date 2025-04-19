7ba8baef5e36ef62a844105787aceb89c7f2449c
coby5502 <coby5502@gmail.com>
2025-04-20 04:56:36 +0900

    [FIX] detail pop error

--- HotSpot/Sources/Presentation/Coordinator/AppCoordinatorView.swift
+++ HotSpot/Sources/Presentation/Coordinator/AppCoordinatorView.swift
@@ -7,44 +7,71 @@ struct AppCoordinatorView: View {
     var body: some View {
         WithViewStore(store, observe: { $0 }) { viewStore in
             NavigationView {
-                VStack {
-                    MapView(store: store.scope(state: \.map, action: \.map))
-                    
-                    NavigationLink(
-                        destination: IfLetStore(
-                            store.scope(state: \.search, action: \.search),
-                            then: { store in
-                                SearchView(store: store)
-                            }
-                        ),
-                        isActive: viewStore.binding(
-                            get: { $0.search != nil },
-                            send: { $0 ? .showSearch : .dismissSearch }
-                        )
-                    ) {
-                        EmptyView()
-                    }
-                    .hidden()
-                    
-                    NavigationLink(
-                        destination: IfLetStore(
-                            store.scope(state: \.shopDetail, action: \.shopDetail),
-                            then: { store in
-                                ShopDetailView(store: store)
-                            }
-                        ),
-                        isActive: viewStore.binding(
-                            get: { $0.isDetailPresented },
-                            send: { $0 ? .showShopDetail(viewStore.selectedShop!) : .dismissDetail }
-                        )
-                    ) {
-                        EmptyView()
-                    }
-                    .hidden()
-                }
+                MapView(store: store.scope(state: \.map, action: \.map))
+                    .background(
+                        Group {
+                            searchNavigationLink(viewStore: viewStore)
+                            mapToDetailNavigationLink(viewStore: viewStore)
+                        }
+                    )
             }
             .navigationViewStyle(.stack)
         }
+    }
+    
+    private func searchNavigationLink(viewStore: ViewStore<AppCoordinator.State, AppCoordinator.Action>) -> some View {
+        NavigationLink(
+            destination: IfLetStore(
+                store.scope(state: \.search, action: \.search),
+                then: { store in
+                    SearchView(store: store)
+                        .background(searchToDetailNavigationLink(viewStore: viewStore))
+                }
+            ),
+            isActive: viewStore.binding(
+                get: { $0.search != nil },
+                send: { $0 ? .showSearch : .dismissSearch }
+            )
+        ) {
+            EmptyView()
+        }
+        .hidden()
+    }
+    
+    private func mapToDetailNavigationLink(viewStore: ViewStore<AppCoordinator.State, AppCoordinator.Action>) -> some View {
+        NavigationLink(
+            destination: IfLetStore(
+                store.scope(state: \.shopDetail, action: \.shopDetail),
+                then: { store in
+                    ShopDetailView(store: store)
+                }
+            ),
+            isActive: viewStore.binding(
+                get: { $0.isDetailPresented && $0.search == nil },
+                send: { $0 ? .showShopDetail(viewStore.selectedShop!) : .dismissDetail }
+            )
+        ) {
+            EmptyView()
+        }
+        .hidden()
+    }
+    
+    private func searchToDetailNavigationLink(viewStore: ViewStore<AppCoordinator.State, AppCoordinator.Action>) -> some View {
+        NavigationLink(
+            destination: IfLetStore(
+                store.scope(state: \.shopDetail, action: \.shopDetail),
+                then: { store in
+                    ShopDetailView(store: store)
+                }
+            ),
+            isActive: viewStore.binding(
+                get: { $0.isDetailPresented && $0.search != nil },
+                send: { $0 ? .showShopDetail(viewStore.selectedShop!) : .dismissDetail }
+            )
+        ) {
+            EmptyView()
+        }
+        .hidden()
     }
 }
 
